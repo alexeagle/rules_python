16fe6f25ef3adb2d132eda2c35b94f618122f6bb
Alex Eagle <alex@aspect.dev>
2021-12-28 16:43:01 -0800

    chore: add black to pre-commit
    Ran pre-commit run --all-files
    We can add this commit to the .git-blame-ignore-revs so it doesn't pollute history.

--- tools/bazel_integration_test/test_runner.py
+++ tools/bazel_integration_test/test_runner.py
@@ -9,45 +9,51 @@ import sys
 import tempfile
 
 from rules_python.python.runfiles import runfiles
+
 r = runfiles.Create()
+
 
 def main(conf_file):
     with open(conf_file) as j:
         config = json.load(j)
 
-    isWindows = platform.system() == 'Windows'
-    bazelBinary = r.Rlocation(os.path.join(config['bazelBinaryWorkspace'], 'bazel.exe' if isWindows else 'bazel'))
-    
-    workspacePath = config['workspaceRoot']
-    # Canonicalize bazel external/some_repo/foo
-    if workspacePath.startswith('external/'):
-        workspacePath = '..' + workspacePath[len('external'):]
+    isWindows = platform.system() == "Windows"
+    bazelBinary = r.Rlocation(
+        os.path.join(
+            config["bazelBinaryWorkspace"], "bazel.exe" if isWindows else "bazel"
+        )
+    )
 
-    with tempfile.TemporaryDirectory(dir = os.environ['TEST_TMPDIR']) as tmpdir:
+    workspacePath = config["workspaceRoot"]
+    # Canonicalize bazel external/some_repo/foo
+    if workspacePath.startswith("external/"):
+        workspacePath = ".." + workspacePath[len("external") :]
+
+    with tempfile.TemporaryDirectory(dir=os.environ["TEST_TMPDIR"]) as tmpdir:
         workdir = os.path.join(tmpdir, "wksp")
         print("copying workspace under test %s to %s" % (workspacePath, workdir))
         shutil.copytree(workspacePath, workdir)
 
-        for command in config['bazelCommands']:
-            bazel_args = command.split(' ')
+        for command in config["bazelCommands"]:
+            bazel_args = command.split(" ")
             try:
-                doubleHyphenPos = bazel_args.index('--')
+                doubleHyphenPos = bazel_args.index("--")
                 print("patch that in ", doubleHyphenPos)
             except ValueError:
                 pass
 
-
-            # Bazel's wrapper script needs this or you get 
+            # Bazel's wrapper script needs this or you get
             # 2020/07/13 21:58:11 could not get the user's cache directory: $HOME is not defined
-            os.environ['HOME'] = str(Path.home())
+            os.environ["HOME"] = str(Path.home())
 
             bazel_args.insert(0, bazelBinary)
-            bazel_process = Popen(bazel_args, cwd = workdir)
+            bazel_process = Popen(bazel_args, cwd=workdir)
             bazel_process.wait()
             if bazel_process.returncode != 0:
                 # Test failure in Bazel is exit 3
                 # https://github.com/bazelbuild/bazel/blob/486206012a664ecb20bdb196a681efc9a9825049/src/main/java/com/google/devtools/build/lib/util/ExitCode.java#L44
                 sys.exit(3)
 
-if __name__ == '__main__':
-  main(sys.argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv[1])
